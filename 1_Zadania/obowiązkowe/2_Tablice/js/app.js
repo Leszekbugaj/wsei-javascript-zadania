ff4627b2a49b43195157eb519614728c54540a13
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-10-06 12:40:19 +0200

    Update app.js

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,19 +1,34 @@
+//Zadanie 0
 function distFronAvarage(tab){
 
-    let sum;
-    for(i=0; i<= tab.length; i++){
+    let sum = 0;
+    let newTab = [];
+
+    for(i=0; i<= tab.length; i++) {
         sum = sum + tab[i];
     }
 
     sum = sum/tab.length;
 
     tab.forEach(el => {
-        el = el-sum;
+        newTab.push(el-sum);
     })
 
-    return tab;
+    return(newTab);
 
 }
 
     let tab = [1,2,3,4,5,6,7,]
+
+    distFronAvarage(tab);
+
+    //zadanie 1
+    var favouriteFruits = ['apple', 'orange', 'bannana'];
+
+    console.log(favouriteFruits[0]);
+    console.log(favouriteFruits[favouriteFruits.length - 1]);
+
+    for (var i=0; i < favouriteFruits.length; i++) {
+        console.log(favouriteFruits[i]);
+    }
 
