8d94d0a6d65c3477ec174fef8efe05dcc1ccf7ce
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-10-20 12:26:42 +0200

    Update app.js

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,82 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+//Task 0
+function checkArray(ddarray) {
+    var result = [];
+    for (let i = 0; i < ddarray.length; i++){
+        const isNotOdd = ddarray[i].filter(number => number % 2 != 0).length > 0;
+        result.push(!isNotOdd);
+    }
+    return result;
+}
+
+checkArray([
+    [11, 12],
+    [42,2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+]);
+
+
+//Zadanie 1
+
+var task1Array = [
+    [2, 3],
+    ["Ala", "Ola"],
+    [true, false],
+    [5, 6, 7, 8],
+    [12, 15, 67]
+];
+
+console.log(task1Array[3][2]);
+
+console.log(task1Array[1].length);
+
+console.log(task1Array[4][2]);
+
+//zadanie 2
+
+
+for (let i = 0; i < task2Array.length; i++){
+    console.log(task2Array[i]);
+}
+
+for (let i = 0; i < task2Array.length; i++){
+    console.log(task2Array[i].length);
+}
+
+for (let i = 0; i < task2Array.length; i++){
+    for (let j = 0; j < task2Array[i].length; j++);{
+    console.log(task2Array[i][j]);
+}
+}
+
+
+//Zadanie 3
+fumction print2DArray(task2Array){
+for (let i = 0; i < task2Array.length; i++){
+    for (let j = 0; j < task2Array[i].length; j++);{
+    console.log(task2Array[i][j]);
+    }
+}
+}
+
+print2DArray([[1,2], [3,4]]);
+
+
+//Zadanie 4
+
+
+var task3Array = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
+print2DArray(task3Array);
+
+//Zadanie 5
+
