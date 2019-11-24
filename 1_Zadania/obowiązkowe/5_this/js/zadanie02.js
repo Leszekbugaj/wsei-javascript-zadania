a2e8b5bde50a2b2540c1e24bd515129eee712428
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-11-24 10:22:59 +0100

    Update zadanie02.js

--- 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
@@ -1,9 +1,26 @@
-var calculator = {
-    save: function (newA, newB) {
-        this.a = newA;
-        this.b = newB;
-    }
 
+var calculator = {
+
+   a: 0,
+   b: 0,
+
+   save: function (newA, newB) {
+
+       this.a = newA;
+       this.b = newB;
+
+   },
+
+  multiply: function() {
+
+       return this.a * this.b;
+
+   },
+
+   sum: function() {
+
+       return this.a + this.b;
+   }
 };
 
 calculator.save(2, 3);
