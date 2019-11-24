fa320c2eb6b274fd6acc9f64dbf02bb4e63d75f7
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-11-24 12:19:38 +0100

    Update app.js

---
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -0,0 +1,12 @@
+function Basket() {
+    products = [];
+    this.sum = 0;
+    this.addProduct = function(name,price){
+        this.products.push({
+            name,
+            price
+        })
+
+    },
+    this.showBasket = 
+}
