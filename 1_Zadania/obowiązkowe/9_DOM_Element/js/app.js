0a031f0c507d30fff89fe37f5a9b568a8ba6fd71
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-12-22 12:25:44 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,90 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+
+//Zadanie 0
+     function getDatasInfo(elements){
+         console.log(elements);
+         let arr = [];
+         for(let i = 0; i < elements.length; i++){
+             arr.push(elements[i].getAttribute("data-color"));
+         }
+         console.log(arr);
+         return arr;
+     }
+     getDatasInfo(links);
 });
+
+//Zadanie 1
+console.log(homeElements);
+console.log(childElements);
+console.log(banner);
+console.log(blocks);
+console.log(links);
+
+for(let i = 0; i < childElements.length; i++){
+    console.log(childElements[i].tagName);
+    console.log(childElements[i].ClassList);
+}
+for(let i = 0; i < blocks.length; i++){
+    console.log(blocks[i].tagName);
+    console.log(blocks[i].ClassList);
+}
+for(let i = 0; i < links.length; i++){
+    console.log(links[i].tagName);
+    console.log(links[i].ClassList);
+}
+
+//Zadanie 2
+for(let i = 0; i < blocks.length; i++){
+    console.log(blocks[i].innerHTML);
+    console.log(blocks[i].outerHTML);
+    blocks[i].innerHTML = 'Nowa wartość diva o klasie blocks'
+}
+
+//Zadanie 3
+var mainFooter = document.getElementById("mainFooter");
+
+function getId(element) {
+    console.log(element);
+    return element.id;
+}
+
+getId(mainFooter);
+
+//Zadanie 4
+function getTags(elements) {
+    let arr = [];
+    for(let i = 0; i < elements.length; i++) {
+        arr.push(elements[i].tagName);
+    }
+    console.log(arr);
+    return arr;
+}
+getTags(childElements);
+
+//Zadanie 5
+function getClassInfo(element){
+    var classes = [];
+    for(let i = 0; i < element.classList.length; i++) {
+        classes.push(element.classList[i]);
+    }
+
+    return classes;
+}
+console.log(getClassInfo(banner));
+
+//Zadanie 6
+const liElements = document.querySelectorAll('nav li');
+const setDataDirection = (elements) => elements.forEach(
+    el => el.getAttribute('data-direction') && el.setAttribute('data-direction', 'top')
+);
+setDataDirection([...liElements]);
+
+function setDataDirection2(elements) {
+    for(let i = 0; i < elements.length; i++){
+        if(elements[i])
+    }
+}
+
+
