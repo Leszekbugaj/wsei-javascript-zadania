1dedd4da279b63b08b19d0bb97062dc8dc925d9a
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2019-12-22 12:38:22 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+     const ex5 = document.getElementsByClassName("ex5")[0];
+
+     const ex5children = ex5children[1].children;
+
+     for(let i = 0; i < ex5children.lemgth; i++) {
+         if(i % 2 === 0)
+            ex5children[i].style.backgroundColor = "green";
+
+         if(i % 3 === 0)
+            ex5.style.textDecoration;
+     }
 });
