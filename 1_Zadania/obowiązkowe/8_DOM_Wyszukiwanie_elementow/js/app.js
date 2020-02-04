94682197618a6887479a50c1ada5a8737a76d48c
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2020-02-04 13:04:07 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    /*
-    Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
-     */
-    //Zadanie #0
+  
+    //Zadanie 0
     const titleEl = document.querySelector("div .title");
 
     function getDataAnimation(element) {
@@ -29,4 +27,38 @@ document.addEventListener("DOMContentLoaded", function () {
     let result = article.getElementByTagName("h1");
     console.log(result.length);
 
+    //Zadanie 1
+    
+    const elementById1 = document.getElementById('home');
+    console.log(elementById1);
+    const elementById2 = document.querySelector('#home');
+    console.log(elementById2);
+
+    const elementByLi = document.getElementsByTagName('li')[0];
+    console.log(elementByLi);
+    
+    const elementByClass = document.getElementsByClassName('block')[0];
+    console.log(elementByClass);
+
+    //Zadanie 2
+    
+    const liElementsInNav = document.getElementsByTagName('nav')[0].getElementsByTagName('li');
+    console.log(liElementsInNav);
+    
+    const divs = document.getElementsByTagName('div');
+    let paragraphs = [];
+    for(let i=0; i<divs.length; i++) {
+        let paragraphInDiv = divs[i].getElementsByTagName('p');
+        for(let j=0; j<paragraphInDiv.length; j++)
+            paragraphs.push(paragraphInDiv[j]);
+    }
+    console.log(paragraphs);
+
+    const articleDivs = document.getElementsByTagName('article')[0].querySelectorAll('div');
+    console.log(articleDivs);
+
+    //Zadanie 3
+    
+    const articleInFirst = document.querySelector('article.first').querySelectorAll('h1');
+    console.log(articleInFirst.length);
 });
