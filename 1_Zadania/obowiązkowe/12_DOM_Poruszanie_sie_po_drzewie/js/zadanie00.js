c18d13ee654971070cf5239583806c6876d558ba
Leszekbugaj <48981350+Leszekbugaj@users.noreply.github.com>
2020-02-05 21:38:41 +0100

    Update zadanie00.js

---
+++ 1_Zadania/obowiązkowe/12_DOM_Poruszanie_sie_po_drzewie/js/zadanie00.js
@@ -0,0 +1,26 @@
+// Zadanie 1
+    var first = document.querySelector('.first');
+    var firstChild = first.firstElementChild;
+    var firstChildThirdChild = first.firstElementChild.children[2];
+
+    // Zadanie 2
+    var second = document.getElementById('second');
+    var secondParent = second.parentElement;
+    var secondParentFirthChild = secondParent.children[3];
+
+    // Zadanie 3
+    var third = document.querySelector('[data-ex="third"]');
+    var finalChildren = third.parentElement.parentElement.lastElementChild.firstElementChild.children;
+    
+    console.log(
+        finalChildren [
+            Math.ceil(finalChildren.length / 2) - 1
+        ]
+     );
+    
+    // Zadanie 4
+    var forth = document.querySelector('.forth');
+    var forthChildren = forth.parentElement;
+    console.log (
+       forthChildren.querySelector('article').querySelectorAll('p')[1]
+    );
